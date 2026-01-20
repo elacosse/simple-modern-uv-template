ff7a739dcec9557290a4f85be7fe203a1f3f42bc
Eric Lacosse <eric.lacosse@mailbox.org>
2026-01-20 23:39:55 +0000

    Add type hint and environment variable to name parameter

--- src/changeme/changeme.py
+++ src/changeme/changeme.py
@@ -5,11 +5,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 def main(name: Annotated[str, typer.Option(help="The name to greet.", envvar="NAME")] = "World"):
     """
     A simple CLI.
     """
     print(f"Hello, {name}!")
+
 
 if __name__ == "__main__":
     typer.run(main)
